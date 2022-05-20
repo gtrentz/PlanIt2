b1b8eb852afcafc104ca1f764b9addbb95fa142a
gtrentz <grant-trentz@uiowa.edu>
2022-05-19 22:44:54 -0500

    Start User.java

---
+++ Objects/User.java
@@ -0,0 +1,8 @@
+public class User {
+    private String fname; private String lname;
+    private int age; private String bday;
+    private final int ID;
+    public User(String f, String l, String b) {
+        fname = f; lname = l; bday = b;
+    }
+}
